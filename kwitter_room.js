746508ba701fdab37df61c7c34069c1ab773888d
tapadiapriyanka21 <85436730+tapadiapriyanka21@users.noreply.github.com>
2023-05-21 17:40:35 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjGJ219LeTDeeCy8utDTdFEC-QTRrHpWY",
+    authDomain: "let-s-chat-web-app-56c9c.firebaseapp.com",
+    projectId: "let-s-chat-web-app-56c9c",
+    storageBucket: "let-s-chat-web-app-56c9c.appspot.com",
+    messagingSenderId: "1054292686664",
+    appId: "1:1054292686664:web:620fe479c613b2ba95a57d"
+  };
